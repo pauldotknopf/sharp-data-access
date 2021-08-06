4f4a03ae84db4d658220ff8f8c380fb0f47e6101
Paul Knopf <pauldotknopf@gmail.com>
2021-08-06 06:27:16 +0000

    Adding unit test project.

---
+++ tests/SharpDataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.Sqlite;
+using SharpDataAccess.Data;
+using SharpDataAccess.Data.Impl;
+using Xunit;
+
+namespace SharpDataAccess.Tests
+{
+    public class UnitTest1
+    {
+        public class TestDbConnectionFactoryProvider : IDbConnectionFactoryProvider
+        {
+            public IDbConnectionFactory BuildConnectionFactory()
+            {
+                return new OrmLiteConnectionFactory(":memory:", new SqliteOrmLiteDialectProvider());
+            }
+        }
+        
+        [Fact]
+        public async Task Test1()
+        {
+            var dataService = new DataService(new TestDbConnectionFactoryProvider(), null, new DataOptions());
+            using (var con = await ConScope.Create(dataService))
+            {
+                await Test(dataService);
+            }
+        }
+
+        public async Task Test(IDataService dataService)
+        {
+            using (var con2 = await ConScope.Create(dataService))
+            {
+                    
+            }
+        }
+    }
+}
